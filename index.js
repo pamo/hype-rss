c6f689cf19fab6a0b3c68304d057d6d5455f2171
Pamela Ocampo <pamela.ocampo@gmail.com>
2016-06-16 22:37:17 -0400

    Retrieve feed and output as xml

---
+++ index.js
@@ -0,0 +1,52 @@
+import http from 'http';
+import RSS from 'rss';
+import pick from 'lodash.pick';
+import forEach from 'lodash.foreach';
+import dropRight from 'lodash.dropright';
+import express from 'express';
+
+const feed = new RSS({
+  title: 'pmocampo\'s favorite tracks on Hype Machine',
+  link: 'http://hypem.com/pmocampo'
+});
+let xml;
+
+const fetchJSON = () => {
+  http.get('http://hypem.com/playlist/loved/pmocampo/json/1/data.js', (response) => {
+    let data = [];
+    response.on('data', (chunk) => {
+      data.push(chunk);
+    });
+    response.on('end', () => {
+      const body = JSON.parse(data.join(''));
+      let transformedResponse = [];
+      forEach(body, (item) => {
+        const track = pick(item, ["artist", "title", "dateloved"]);
+        const title = `${track.artist} - ${track.title}`;
+        feed.item({
+          title,
+          date: new Date(track.dateloved * 1000)
+        });
+      });
+      xml = feed.xml({
+        indent: true
+      });
+      console.log(xml);
+    });
+  }).on('error', (e) => {
+    console.log(`Got error: ${e.message}`);
+  });
+};
+
+var app = express();
+
+app.get('/', function(req, res) {
+  fetchJSON();
+  res.set('Content-Type', 'application/rss+xml');
+  res.send(xml);
+});
+
+const port = 8080;
+app.listen(port, function() {
+  console.log(`Listening on port ${port}!`);
+});
---
+++ rss.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var _http = require('http');
+
+var _http2 = _interopRequireDefault(_http);
+
+var _rss = require('rss');
+
+var _rss2 = _interopRequireDefault(_rss);
+
+var _lodash = require('lodash.pick');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+var _lodash3 = require('lodash.foreach');
+
+var _lodash4 = _interopRequireDefault(_lodash3);
+
+var _lodash5 = require('lodash.dropright');
+
+var _lodash6 = _interopRequireDefault(_lodash5);
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var feed = new _rss2.default({
+  title: 'pmocampo\'s favorite tracks on Hype Machine',
+  link: 'http://hypem.com/pmocampo'
+});
+var xml = void 0;
+
+var fetchJSON = function fetchJSON() {
+  _http2.default.get('http://hypem.com/playlist/loved/pmocampo/json/1/data.js', function (response) {
+    var data = [];
+    response.on('data', function (chunk) {
+      data.push(chunk);
+    });
+    response.on('end', function () {
+      var body = JSON.parse(data.join(''));
+      var transformedResponse = [];
+      (0, _lodash4.default)(body, function (item) {
+        var track = (0, _lodash2.default)(item, ["artist", "title", "dateloved"]);
+        var title = track.artist + ' - ' + track.title;
+        feed.item({
+          title: title,
+          date: new Date(track.dateloved * 1000)
+        });
+      });
+      xml = feed.xml({
+        indent: true
+      });
+      console.log(xml);
+    });
+  }).on('error', function (e) {
+    console.log('Got error: ' + e.message);
+  });
+};
+
+var app = (0, _express2.default)();
+
+app.get('/', function (req, res) {
+  fetchJSON();
+  res.set('Content-Type', 'application/rss+xml');
+  res.send(xml);
+});
+
+var port = 8080;
+app.listen(port, function () {
+  console.log('Listening on port ' + port + '!');
+});
